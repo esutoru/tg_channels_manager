e9676c5f9e64506115e1341dce5f7e41494c05b2
vlad <irenfri@gmail.com>
2022-10-22 19:16:54 +0300

    feat: Update lints config

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,6 @@
-from backend.src.example.views import router as example_router
 from fastapi import APIRouter
+
+from backend.src.example.views import router as example_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(example_router, prefix="/example", tags=["Example"])
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
+from fastapi import FastAPI
+
 from backend.src.api import api_router
 from backend.src.config import settings
-from fastapi import FastAPI
 
 app = FastAPI(
     title="Telegram Channels Manager's API",
